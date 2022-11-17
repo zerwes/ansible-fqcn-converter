1a1402dd64b6f06856ca2bace416ed5045c3d6c4
Klaus Zerwes <zerwes@users.noreply.github.com>
2022-11-17 11:54:12 +0100

    Revert "fixed taskstartregex ..." reverted (reopen #11)
    This reverts commit 6ea8bd799db883f19ca7a9885aded1551db82889.

--- fqcn-fixer.py
+++ fqcn-fixer.py
@@ -296,7 +296,7 @@ for dirpath, dirnames, files in os.walk(os.path.abspath(args.directory)):
 # prepare regex
 _fqcnregex = re.compile(r'^(?P<white>\s*-?\s+)(?P<module>%s):' % '|'.join(fqcndict.keys()))
 _taskstartregex = re.compile(
-    r'^(?P<white>\s*-\s+)(?P<nm>%s):$' %
+    r'^(?P<white>\s*-\s+)(?P<nm>%s):' %
         '|'.join(['name'] + list(fqcndict.keys()))
     )
 
